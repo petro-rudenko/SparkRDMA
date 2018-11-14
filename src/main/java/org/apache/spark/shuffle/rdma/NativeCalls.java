03d9bddf85b313cd86107ec22cf59edba259a339
Peter Rudenko <peterr@mellanox.com>
2018-11-14 18:10:46 +0200

    Add netive calls interface
    Change-Id: I1af5c1294148654fc5f88257e49a2c37028bd45b

--- src/main/java/org/apache/spark/shuffle/rdma/NativeCalls.java
+++ src/main/java/org/apache/spark/shuffle/rdma/NativeCalls.java
@@ -1,4 +1,10 @@
 package org.apache.spark.shuffle.rdma;
 
 public class NativeCalls {
+  static
+  {
+    System.loadLibrary("spark-rdma-native");
+  }
+
+  native void writeIndexFileAndCommit(int shuffleId, int mapId, long[] lengths, String dataTmp);
 }
